81f90c9dcba133281d5962bf4f5b9d9abe3af21d
Stephen Sheafer <ssheafer@purdue.edu>
2012-09-02 13:59:00 -0400

    Your Active games now shows only games where you are alive, and Defeated and Defeated this turn now update properly

--- app/models/game.rb
+++ app/models/game.rb
@@ -82,8 +82,8 @@ class Game < ActiveRecord::Base
             turn = submitted[id]
             if status == 1 then signupsByNation[id].status = "Alive" end
             if status == 2 then signupsByNation[id].status = "AI" end
-            if status == 0xfe then signupsByNation[id].status = "Defeated" end
-            if status == 0xff then signupsByNation[id].status = "Defeated_This_Turn" end
+            if status == 0xfe then signupsByNation[id].status = "Defeated_This_Turn" end
+            if status == 0xff then signupsByNation[id].status = "Defeated" end
             signupsByNation[id].turn_cd = turn
             signupsByNation[id].save
           end
